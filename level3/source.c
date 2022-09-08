36dd71433a51dad2434dd8f68f3868b82b0a36ff
Julien Janin-Reynaud <jjanin-r@student.42lyon.fr>
2022-09-08 14:07:49 +0200

    Update source.c

--- level3/source.c
+++ level3/source.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int		m = 0;
+
+int		v()
+{
+	char	buffer[520];
+
+	fgets(buffer, 512, stdin);
+	printf(buffer);
+	if (m == 64)
+	{
+		fwrite("Wait what?!\n", 12, 1, stdout);
+		system("/bin/sh");
+	}
+	return (0);
+}
+
+int		main(int argc, char **argv)
+{
+	v();
+	return (0);
+}
