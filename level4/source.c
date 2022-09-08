6909a38a176d168183d3fb1f346eb91a9d72b786
Julien Janin-Reynaud <jjanin-r@student.42lyon.fr>
2022-09-08 18:37:42 +0200

    Update source.c

--- level4/source.c
+++ level4/source.c
@@ -1,1 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int m = 0;
+
+int p(char *buffer)
+{
+	printf(buffer);
+	return (0);
+}
+
+int n()
+{
+	char	buffer[520];
+
+	fgets(buffer, 512, stdin);
+	p(buffer);
+	if (m == 16930116) {
+    system("/bin/cat /home/user/level5/.pass");
+  }
+	return (0);
+}
+
+int main(int argc, char **argv)
+{
+	n();
+	return (0);
+}
