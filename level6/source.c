903db6d08726228f831490a0f3600ba630090dc4
Julien Janin-Reynaud <jjanin-r@student.42lyon.fr>
2022-09-09 15:02:52 +0200

    Update source.c

---
+++ level6/source.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+void n(void) {
+  system("/bin/cat /home/user/level7/.pass");
+}
+
+void m(void) {
+  puts("Nope");
+}
+
+int	main(int argc, char **argv)
+{
+	char		*str;
+	void (*ptr)(void);
+
+	str = malloc(64);
+	ptr = malloc(4);
+	ptr = &m;
+
+	strcpy(str, argv[1]);
+	ptr();
+
+	return (0);
+}
