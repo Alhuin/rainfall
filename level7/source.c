b057b37253e41ff5f2ee8c7b0f5743e2082dc6fb
Julien Janin-Reynaud <jjanin-r@student.42lyon.fr>
2022-09-30 14:23:23 +0200

    Update source.c with debugging for walkthrough

--- level7/source.c
+++ level7/source.c
@@ -20,7 +20,18 @@ int main(int argc, char **argv) {
   b[0] = 2;
   b[1] = malloc(8);
   
+  /*            Debugging for walkthrough
+  printf("\na\t(%p)\t=\t%p\na[0]\t(%p)\t=\t%d\na[1]\t(%p)\t=\t%p\n\n", &a, a, &a[0], a[0], &a[1], a[1]);
+  printf("\nb\t(%p)\t=\t%p\nb[0]\t(%p)\t=\t%d\nb[1]\t(%p)\t=\t%p\n\n", &b, b, &b[0], b[0], &b[1], b[1]);
+  printf("strcpy(%p, %s)\n", a[1], av[1]);
+  */
   strcpy(a[1], argv[1]);
+  
+  /*
+  printf("\na\t(%p)\t=\t%p\na[0]\t(%p)\t=\t%d\na[1]\t(%p)\t=\t%p\n\n", &a, a, &a[0], a[0], &a[1], a[1]);
+  printf("\nb\t(%p)\t=\t%p\nb[0]\t(%p)\t=\t%d\nb[1]\t(%p)\t=\t%p\n\n", &b, b, &b[0], b[0], &b[1], b[1]);
+  printf("strcpy(%p, %s)\n", a[1], av[1]);
+  */
   strcpy(b[1], argv[2]);
   
   fgets(c, 68, fopen("/home/user/level8/.pass", "r"));
