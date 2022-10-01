aa10397e907bd5f890347afe78a48a2ea8c61070
Alhuin <jjanin-r@student.42lyon.fr>
2022-10-01 13:22:35 +0200

    init level1

---
+++ bonus1/source.c
@@ -0,0 +1,12 @@
+int main(int argc, char **argv)
+{
+    char b[40];
+
+    a = atoi(argv[1]);
+    if (a > 9)
+        return (1);
+    memcpy(b, argv[2], a*4);
+    if (a == 1464814662)
+        execl("/bin/sh", "sh", NULL);
+    return(0);
+}
